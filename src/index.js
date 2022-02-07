a7d54a9b5a5d2ca608e000eef3231e10d3d2b7de
Alex <balanoleksyy@gmail.com>
2022-02-07 13:39:30 +0200

    added element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const e = (
+  <div>
+    <h1>My Todo List</h1>
+    <input placeholder="search" />
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(e, document.getElementById('root'));
